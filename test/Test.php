f5b149cb2ae0505df417555092cceb60fe0a39db
thinkPad-myj <815081410@qq.com>
2021-01-25 17:04:57 +0800

    '第一次提交测试'

---
+++ test/Test.php
@@ -0,0 +1,18 @@
+<?php
+// +----------------------------------------------------------------------
+// | CoolCms [ DEVELOPMENT IS SO SIMPLE ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2020 http://www.coolcms.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +---------------------------------------------------------------------
+// | Author: Myj <815081410@qq.com>
+// +----------------------------------------------------------------------
+namespace Test;
+
+require_once "../vendor/autoload.php";
+
+use Ding\ding\DingTest;
+
+$a = new DingTest();
+$a->index();
